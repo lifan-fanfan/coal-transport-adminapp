7d532077984204fdd41f1758781e039036320edd
lifan <75855385@qq.com>
2019-07-12 16:43:52 +0800

    创建登录页面

---
+++ pages/login/login.vue
@@ -0,0 +1,46 @@
+<template>
+	<view>
+		<view class="username-wrap">
+			用户名：<input type="text" class="username" v-model="userName" />
+		</view>
+		<view class="password-wrap">
+			密码：<input type="password" class="password" v-model="passWord" />
+		</view>
+		<view class="">
+			<button @click="submitEvent">登录</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				userName: '',
+				passWord: ''
+			};
+		},
+		methods: {
+			submitEvent() {
+				console.log(this.userName)
+				console.log(this.passWord)
+			}
+		},
+	}
+</script>
+
+<style lang="less">
+.username-wrap {
+	.username {
+		float: right;
+		border: 1px solid #eee;
+	}
+	
+}
+.password-wrap {
+	.password {
+		float: right;
+		border: 1px solid #eee;
+	}
+}
+</style>
